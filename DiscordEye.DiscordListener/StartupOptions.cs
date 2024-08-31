0c91f01a48c94fc1ddcc3ae321068228800d0bae
Daniel <89273037+7645re@users.noreply.github.com>
2024-09-01 02:33:24 +0300

    fix

--- DiscordEye.DiscordListener/StartupOptions.cs
+++ DiscordEye.DiscordListener/StartupOptions.cs
@@ -2,6 +2,7 @@ namespace DiscordEye.DiscordListener;
 
 public class StartupOptions
 {
+    public string Token { get; set; }
     public int MessageCacheSize { get; set; }
     public bool SendEvents { get; set; }
 }
